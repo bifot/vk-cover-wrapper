d639dba1ec45b2ace4eb5312289b7c90c0444901
bifot <bifot@bifot.ru>
2017-09-02 00:11:55 +0300

    refactor

--- method/uploadAndSaveCoverPhoto.js
+++ method/uploadAndSaveCoverPhoto.js
@@ -4,16 +4,10 @@ const saveCoverPhoto = require('./saveCoverPhoto')
 
 module.exports = (groupId, groupToken, coverPath) => {
   return new Promise((resolve, reject) => {
-    getUploadServer(groupId, groupToken)
-      .then((uploadServer) => {
-        uploadCoverPhoto(uploadServer, coverPath)
-          .then((coverData) => {
-            saveCoverPhoto(Object.assign({}, coverData, { access_token: groupToken }))
-              .then(resolve)
-              .catch(reject)
-          })
-          .catch(reject)
-      })
+    return getUploadServer(groupId, groupToken)
+      .then(uploadServer => uploadCoverPhoto(uploadServer, coverPath))
+      .then(coverData => saveCoverPhoto(Object.assign({}, coverData, { access_token: groupToken })))
+      .then(resolve)
       .catch(reject)
   })
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const { uploadAndSaveCoverPhoto } = require('../index')
 
+console.log(process.env.GROUP_ID, process.env.GROUP_TOKEN, process.env.COVER_PATH)
+
 uploadAndSaveCoverPhoto(process.env.GROUP_ID, process.env.GROUP_TOKEN, process.env.COVER_PATH)
   .then(console.log)
   .catch(console.error)
